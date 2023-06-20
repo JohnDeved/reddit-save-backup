08ab9b2332367a29716b096247a63d27b15244fa
Johann Berger <johann@objekt.stream>
2023-06-20 09:36:13 +0200

    + recheck skipped urls

---
+++ scripts/fetchOldSaves.ts
@@ -0,0 +1,11 @@
+import { writeFileSync } from 'fs'
+import stored from '../stored.json'
+
+fetch('https://raw.githubusercontent.com/JohnDeved/reddit-save-backup/de5edba5aaa5b9b886425127ce0d3e07c8fd70cb/old.saved.json')
+  .then(res => res.json())
+  .then(async (names: string[]) => {
+    // filter out already saved
+    const storedNames = stored.map(s => s.name)
+    const unStoredNames = names.filter(n => !storedNames.includes(n))
+    writeFileSync('./old.saved.json', JSON.stringify(unStoredNames, null, 2))
+  })
